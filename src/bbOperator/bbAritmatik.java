e51810fbff7347695a41864cbfb1fd13edd61f92
boenkkk <sebuah.email@gmail.com>
2019-01-13 21:44:20 +0700

    Operator

---
+++ src/bbOperator/bbAritmatik.java
@@ -0,0 +1,21 @@
+package bbOperator;
+
+public class bbAritmatik {
+	public static void main(String args[]){
+		int x = 10;
+		x = x + 1; //bentuk paling panjang
+		x += 1; //menggunakan operator gabungan +=
+		x++; //menggunakan operator increment
+		System.out.println("x = "+x); //13
+		
+		int y = 10;
+		y = y - 1; //bentuk paling panjang
+		y -= 1; //menggunakan operator gabungan -=
+		y--; //menggunakan operator decrement
+		System.out.println("y = "+y); //7
+		
+		int a = 10;
+		a *= 2;
+		System.out.println("a = "+a);
+	}
+}
