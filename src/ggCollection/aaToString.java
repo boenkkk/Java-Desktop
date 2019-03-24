8ef45198478103e1b72f39676b4f8da7978011dc
boenkkk <sebuah.email@gmail.com>
2019-03-24 19:12:22 +0700

    Collection - toString

---
+++ src/ggCollection/aaToString.java
@@ -0,0 +1,8 @@
+package ggCollection;
+
+public class aaToString {
+	public static void main(String[] args) {
+		aaToString test = new aaToString();
+		System.out.println("implementasi toString dari class Object " + "menghasilkan : " + test);
+	}
+}
